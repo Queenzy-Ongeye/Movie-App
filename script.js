d0ca6ea8da0c68117b157a770097416be579f81f
Queen-02 <queenzyblack176@gmail.com>
2022-07-26 08:55:13 +0300

    added genre tags

--- script.js
+++ script.js
@@ -8,91 +8,171 @@ const searchURL = BASE_URL + '/search/movie?' + API_KEY;
 const main = document.getElementById('main');
 const form = document.getElementById('form');
 const search = document.getElementById('search');
-getMovie(API_URL);
+const tagsEl = document.getElementById('tags');
 
 
-const genre = [
+
+
+const genres = [
     {
-       "id":28,
-       "name":"Action"
+        "id": 28,
+        "name": "Action"
     },
     {
-       "id":12,
-       "name":"Adventure"
+        "id": 12,
+        "name": "Adventure"
     },
     {
-       "id":16,
-       "name":"Animation"
+        "id": 16,
+        "name": "Animation"
     },
     {
-       "id":35,
-       "name":"Comedy"
+        "id": 35,
+        "name": "Comedy"
     },
     {
-       "id":80,
-       "name":"Crime"
+        "id": 80,
+        "name": "Crime"
     },
     {
-       "id":99,
-       "name":"Documentary"
+        "id": 99,
+        "name": "Documentary"
     },
     {
-       "id":18,
-       "name":"Drama"
+        "id": 18,
+        "name": "Drama"
     },
     {
-       "id":10751,
-       "name":"Family"
+        "id": 10751,
+        "name": "Family"
     },
     {
-       "id":14,
-       "name":"Fantasy"
+        "id": 14,
+        "name": "Fantasy"
     },
     {
-       "id":36,
-       "name":"History"
+        "id": 36,
+        "name": "History"
     },
     {
-       "id":27,
-       "name":"Horror"
+        "id": 27,
+        "name": "Horror"
     },
     {
-       "id":10402,
-       "name":"Music"
+        "id": 10402,
+        "name": "Music"
     },
     {
-       "id":9648,
-       "name":"Mystery"
+        "id": 9648,
+        "name": "Mystery"
     },
     {
-       "id":10749,
-       "name":"Romance"
+        "id": 10749,
+        "name": "Romance"
     },
     {
-       "id":878,
-       "name":"Science Fiction"
+        "id": 878,
+        "name": "Science Fiction"
     },
     {
-       "id":10770,
-       "name":"TV Movie"
+        "id": 10770,
+        "name": "TV Movie"
     },
     {
-       "id":53,
-       "name":"Thriller"
+        "id": 53,
+        "name": "Thriller"
     },
     {
-       "id":10752,
-       "name":"War"
+        "id": 10752,
+        "name": "War"
     },
     {
-       "id":37,
-       "name":"Western"
+        "id": 37,
+        "name": "Western"
     }
- ]
+]
+
+var selectedGenre = [];
+setGenres();
+function setGenres() {
+    tagsEl.innerHTML = '';
+    genres.forEach(genre => {
+        const t = document.createElement('div');
+        t.classList.add('tag');
+        t.id = genre.id;
+        t.innerText = genre.name;
+        t.addEventListener('click', () => {
+            if (selectedGenre.length == 0) {
+                selectedGenre.push(genre.id);
+            } else {
+                if (selectedGenre.includes(genre.id)) {
+                    selectedGenre.forEach((id, index) => {
+                        if (id == genre.id) {
+                            selectedGenre.splice(index, 1);
+                        }
+                    })
+                } else {
+                    selectedGenre.push(genre.id);
+                }
+            }
+            console.log(selectedGenre)
+            getMovie(API_URL + '&with_genres=' + encodeURI(selectedGenre.join(',')));
+            highlightSelections()
+
+        })
+        tagsEl.appendChild(t);
+    })
+
+
+}
+
+function highlightSelections() {
+    const tags = document.querySelectorAll('.tag')
+    tags.forEach(tag => {
+        tag.classList.remove('highlight')
+    })
+    clearBTN()
+    if (selectedGenre.length != 0) {
+        selectedGenre.forEach(id => {
+            const highlightedTag = document.getElementById(id);
+            highlightedTag.classList.add('highlight');
+        })
+    }
+}
+
+function clearBTN() {
+
+    let clearbtn = document.getElementById('clear');
+    if (clearbtn) {
+        clearbtn.classList.add('highlight');
+
+    } else {
+        let clear = document.createElement('div');
+        clear.classList.add('tag', 'highlight');
+        clear.id = 'clear';
+        clear.innerText = 'clear';
+
+        clear.addEventListener('click', () => {
+            selectedGenre = [];
+            setGenres();
+            getMovie(API_URL);
+        })
+        tagsEl.append(clear);
+    }
+
+
+}
+
+getMovie(API_URL);
 function getMovie(url) {
     fetch(url).then(response => response.json()).then(data => {
         // console.log(data);
-        showMovies(data.results);
+        if (data.results.length !== 0) {
+            showMovies(data.results);
+        } else {
+            main.innerHTML = '<h1 class = "noresult">NO RESULT FOUND</h1>'
+        }
+
     })
 }
 
@@ -105,7 +185,7 @@ function showMovies(data) {
         const movieEl = document.createElement('div');
         movieEl.classList.add('movie');
         movieEl.innerHTML = `
-            <img src="${IMAGE_URL + poster_path}" alt="${title}">
+            <img src="${poster_path ? IMAGE_URL + poster_path : 'https://via.placeholder.com/150'}" alt="${title}">
             <div class="movie-info">
                 <h3>${title}</h3>
                 <span class="${getColor(vote_average)}"></span>${vote_average}</span>
@@ -137,10 +217,12 @@ form.addEventListener('submit', (e) => {
     e.preventDefault();
 
     const searchTerm = search.value;
+    selectedGenre = [];
+    setGenres();
 
-    if (searchTerm){
+    if (searchTerm) {
         getMovie(searchURL + '&query=' + searchTerm);
-    }else{
+    } else {
         getMovie(API_URL);
     }
 })
